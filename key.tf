96fd22244ad638782b081de5b433afcefb917906
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-01 12:44:00 -0500

    test

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "ELK" {
-  key_name   = "ELK"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
+// resource "aws_key_pair" "ELK" {
+//   key_name   = "ELK"
+//   public_key = "${file("~/.ssh/id_rsa.pub")}"
+// }
